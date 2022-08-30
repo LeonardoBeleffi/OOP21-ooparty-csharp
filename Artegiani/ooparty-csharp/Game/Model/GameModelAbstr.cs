0627d535d3730675fc61339f645fdd20a6c293b7
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-08-30 15:42:25 +0200

    Add GameModel

---
+++ Artegiani/ooparty-csharp/Game/Model/GameModelAbstr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ooparty_csharp.Game.Player;
+
+namespace ooparty_csharp.Game.Model
+{
+
+
+    /// <summary>
+    /// Abstract implementation of <see cref="IGameModel"/>.
+    /// </summary>
+    public abstract class GameModelAbstr : IGameModel
+    {
+        public GameModelAbstr(List<IPlayer> players)
+        {
+            Players = players;
+            PlayerEnumerator = Players.GetEnumerator();
+            CurrPlayer = null;
+        }
+
+        public List<IPlayer> Players { get; private set; }
+
+        public IPlayer CurrPlayer
+        {
+            get => CurrPlayer;
+            private set => CurrPlayer = (IPlayer)PlayerEnumerator.Current;
+        }
+
+        public abstract bool RunGame();
+
+        /// <summary>
+        /// <c>PlayerEnumerator</c> represents the enumerator of the list of players.
+        /// </summary>
+        protected IEnumerator PlayerEnumerator { set; get; }
+    }
+}
---
+++ Artegiani/ooparty-csharp/Game/Model/IGameModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using ooparty_csharp.Game.Player;
+
+namespace ooparty_csharp.Game.Model
+{
+    /// <summary>
+    /// This interface models the game model.
+    /// </summary>
+    public interface IGameModel
+    {
+        /// <summary>
+        /// This method runs the game as long as there are turns to do.
+        /// </summary>
+        /// <returns>True if there is another turn to play</returns>
+        bool RunGame();
+
+        /// <summary>
+        /// <c>Players</c> represents the list of players.
+        /// </summary>
+        List<IPlayer> Players { get; }
+
+        /// <summary>
+        /// <c>CurrPlayer</c> represents the current player.
+        /// </summary>
+        IPlayer CurrPlayer { get; }
+    }
+}
