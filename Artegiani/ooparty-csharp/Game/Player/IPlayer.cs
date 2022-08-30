b95ad8c39bd78a35cf83aa7ac20c4c19f672ba2f
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-08-30 15:18:05 +0200

    Add Player

---
+++ Artegiani/ooparty-csharp/Game/Player/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace ooparty_csharp.Game.Player
+{
+    /// <summary>
+    /// This interface models a player.
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// <c>Nickname</c> represents the nickname of the player.
+        /// </summary>
+        string Nickname { get; }
+    }
+}
---
+++ Artegiani/ooparty-csharp/Game/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace ooparty_csharp.Game.Player
+{
+    /// <summary>
+    /// Implementation of <see cref="IPlayer"/>.
+    /// </summary>
+    public class Player : IPlayer
+    {
+        /// <summary>
+        /// Builds a <see cref="Player"/>.
+        /// </summary>
+        /// <param name="nickname">The nickname of the player.</param>
+        public Player(string nickname)
+        {
+            Nickname = nickname;
+        }
+
+        public string Nickname { get; private set; }
+    }
+}
