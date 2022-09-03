789b764921ece490c526ee3d1b2681c08813b6bb
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-09-04 00:10:50 +0200

    Add empty AbstractMinigameModel to remove import errors

---
+++ Beleffi/Minigames/Common/Model/AbstractMinigameModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Beleffi.Game.Dice.Model;
+using Beleffi.Game.Player;
+
+namespace Beleffi.Minigames.Common.Model
+{
+    public class AbstractMinigameModel
+    {
+        public AbstractMinigameModel(in IList<IPlayer> players, in IDiceModel dice)
+        {
+        }
+    }
+}
