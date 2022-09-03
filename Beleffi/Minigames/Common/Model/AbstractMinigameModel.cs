205cb6cb8abe14b89f9fd61109c3e1ecae9fc46e
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-09-04 01:16:26 +0200

    Add empty methods to fix method-call errors

--- Beleffi/Minigames/Common/Model/AbstractMinigameModel.cs
+++ Beleffi/Minigames/Common/Model/AbstractMinigameModel.cs
@@ -9,5 +9,38 @@ namespace Beleffi.Minigames.Common.Model
         public AbstractMinigameModel(in IList<IPlayer> players, in IDiceModel dice)
         {
         }
+
+        public int Score { get; set; }
+        
+        public bool HasCurrPlayer()
+        {
+            return false;
+        }
+
+        public void SetGameResults()
+        {
+        }
+
+        public bool HasNextPlayer()
+        {
+            return false;
+        }
+        
+        public void SetPlayerIterator(in IList<IPlayer> players)
+        {
+        }
+
+        public IList<IPlayer> GetPlayers()
+        {
+            return new List<IPlayer>();
+        }
+        
+        public void SetCurrPlayer()
+        {
+        }
+
+        public void ScoreMapper(in IPlayer player, in int score)
+        {
+        }
     }
 }
