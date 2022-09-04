c9c943841ee75d9102688c0d430c1f3de5982b76
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-09-04 02:29:01 +0200

    Add interface extension and methods to remove errors

--- Beleffi/Minigames/Memo/View/IMemoViewController.cs
+++ Beleffi/Minigames/Memo/View/IMemoViewController.cs
@@ -1,6 +1,13 @@
+using System.Collections.Generic;
+using Beleffi.Game.Player;
+using Beleffi.Utils.View;
+
 namespace Beleffi.Minigames.Memo.View
 {
-    public interface IMemoViewController
+    public interface IMemoViewController : IGenericViewController
     {
+        void Start(in IList<int> cards);
+
+        void SetPlayerLabelText(in IPlayer player);
     }
 }
