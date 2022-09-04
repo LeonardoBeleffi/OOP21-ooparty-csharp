6d96dc372d165ad77ef5347c728e2f3c0504580e
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-09-04 02:20:44 +0200

    Fix type parameter name

--- Beleffi/Minigames/Common/Controller/AbstractMinigameController.cs
+++ Beleffi/Minigames/Common/Controller/AbstractMinigameController.cs
@@ -3,9 +3,19 @@ using Beleffi.Utils.Graphics.Controller;
 
 namespace Beleffi.Minigames.Common.Controller
 {
-    public class AbstractMinigameController<S>
+    public class AbstractMinigameController<TS>
     {
-        public AbstractMinigameController(in IStageManager<S> s, in IDiceController dice)
+        public AbstractMinigameController(in IStageManager<TS> s, in IDiceController dice)
+        {
+        }
+
+        public IStageManager<TS> GetStageManager()
+        {
+            return null;
+        }
+
+
+        public void CloseGame()
         {
         }
     }
