9dc480da9b65a42d30da17379063a90ad25af70e
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-09-04 02:27:13 +0200

    Create interface IMinigameModel

--- Beleffi/Minigames/Common/Model/IMinigameModel.cs
+++ Beleffi/Minigames/Common/Model/IMinigameModel.cs
@@ -1,6 +1,30 @@
+using System.Collections.Generic;
+using Beleffi.Game.Player;
+
 namespace Beleffi.Minigames.Common.Model
 {
     public interface IMinigameModel
     {
+        int Score { get; set; }
+
+        bool HasCurrPlayer();
+
+        IList<IPlayer> GetGameResults();
+ 
+        void SetGameResults();
+
+        bool HasNextPlayer();
+
+        void SetPlayerIterator(in IList<IPlayer> players);
+
+        IList<IPlayer> GetPlayers();
+
+        IPlayer GetCurrPlayer();
+
+        void SetCurrPlayer();
+
+        void ScoreMapper(in IPlayer player, in int score);
+
+        bool RunGame();
     }
 }
